2a4622de61b92673256afd3cbdfca05ea6736869
doodlegoo <vincent.gagnon5@gmail.com>
2015-09-30 15:29:04 -0400

    dev2

--- Devoir2/Devoir2_Sorties.R
+++ Devoir2/Devoir2_Sorties.R
@@ -10,9 +10,18 @@ setup <- function() {
 numero1 <- function() {
   impactEchant = setup()
   impactPop = suppressWarnings(read.spss("/home/vincent/Github/mqg811/Devoir2/ImpactPromo.sav", to.data.frame=TRUE))  
-  print( summary(impactEchant$Region)/sum(table(impactEchant$Region)) )
-  print( summary(impactPop$Region)/sum(table(impactPop$Region)) )
-  
+  propEchant = summary(impactEchant$Region)/sum(table(impactEchant$Region))
+  propPop = summary(impactPop$Region)/sum(table(impactPop$Region))
+  diffRegion1 = propEchant["Région 1"] - propPop["Région 1"]
+  diffRegion2 = propEchant["Région 2"] - propPop["Région 2"]
+  diffRegion3 = propEchant["Région 3"] - propPop["Région 3"]
+  diffRegion4 = propEchant["Région 4"] - propPop["Région 4"]
+  diffRegion5 = propEchant["Région 5"] - propPop["Région 5"]
+  print(diffRegion1 )
+  print(diffRegion2 )
+  print(diffRegion3 )
+  print(diffRegion4 )
+  print(diffRegion5 )
 }
 
 numero2 <- function() {
