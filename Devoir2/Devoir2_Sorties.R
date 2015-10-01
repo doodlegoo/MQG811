5d898c1d5353cd4e967c0ef68b23eeae23ce758a
doodlegoo <vincent.gagnon5@usherbrooke.ca>
2015-09-30 21:54:25 -0400

    Num 1 terminé

--- Devoir2/Devoir2_Sorties.R
+++ Devoir2/Devoir2_Sorties.R
@@ -1,7 +1,8 @@
 setup <- function() {
   options(warn=-1)
   library(foreign)
-  dataset = suppressWarnings(read.spss("/home/vincent/Github/mqg811/Devoir2/ImpactPromo_900.sav", to.data.frame=TRUE))  
+  # dataset = suppressWarnings(read.spss("/home/vincent/Github/mqg811/Devoir2/ImpactPromo_900.sav", to.data.frame=TRUE)) 
+  dataset = suppressWarnings(read.spss("~/Rstudio/STT811/mqg811/Devoir2/ImpactPromo_900.sav", to.data.frame=TRUE))
   return( dataset)
   
   
@@ -9,14 +10,15 @@ setup <- function() {
 
 numero1 <- function() {
   impactEchant = setup()
-  impactPop = suppressWarnings(read.spss("/home/vincent/Github/mqg811/Devoir2/ImpactPromo.sav", to.data.frame=TRUE))  
+  # impactPop = suppressWarnings(read.spss("/home/vincent/Github/mqg811/Devoir2/ImpactPromo.sav", to.data.frame=TRUE)) 
+  impactPop = suppressWarnings(read.spss("~/Rstudio/STT811/mqg811/Devoir2/ImpactPromo.sav", to.data.frame=TRUE))
   propEchant = summary(impactEchant$Region)/sum(table(impactEchant$Region))
   propPop = summary(impactPop$Region)/sum(table(impactPop$Region))
-  diffRegion1 = propEchant["Région 1"] - propPop["Région 1"]
-  diffRegion2 = propEchant["Région 2"] - propPop["Région 2"]
-  diffRegion3 = propEchant["Région 3"] - propPop["Région 3"]
-  diffRegion4 = propEchant["Région 4"] - propPop["Région 4"]
-  diffRegion5 = propEchant["Région 5"] - propPop["Région 5"]
+  diffRegion1 = abs(propEchant["Région 1"]- propPop["Région 1"])
+  diffRegion2 = abs(propEchant["Région 2"] - propPop["Région 2"])
+  diffRegion3 = abs(propEchant["Région 3"] - propPop["Région 3"])
+  diffRegion4 = abs(propEchant["Région 4"] - propPop["Région 4"])
+  diffRegion5 = abs(propEchant["Région 5"] - propPop["Région 5"])
   print(diffRegion1 )
   print(diffRegion2 )
   print(diffRegion3 )
