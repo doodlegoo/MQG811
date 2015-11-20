4e78a685a3c1ec755ee8880cf311e6e5ab53f21d
doodlegoo <vincent.gagnon5@gmail.com>
2015-11-19 20:54:11 -0500

    Partie 1 terminé

--- Devoir3/Devoir3_Sorties.R
+++ Devoir3/Devoir3_Sorties.R
@@ -1,29 +1,56 @@
 setup <- function() {
   options(warn=-1)
   library(foreign)
-  dataset = suppressWarnings(read.spss(file.path(getwd(),"Devoir 3/Devoir3_Cirque.sav"), to.data.frame=TRUE))
-#    dataset = suppressWarnings(read.spss("~/Rstudio/STT811/mqg811/Devoir2/ImpactPromo_900.sav", to.data.frame=TRUE))
+  #dataset = suppressWarnings(read.spss(file.path(getwd(),"Devoir3/Devoir3_Cirque.sav"), to.data.frame=TRUE))
+#  dataset = suppressWarnings(read.spss("~/Rstudio/STT811/mqg811/Devoir3/Devoir3_Cirque.sav", to.data.frame=TRUE))
+  dataset = suppressWarnings(read.spss("/home/vincent/Github/mqg811/Devoir3/Devoir3_Cirque.sav", to.data.frame=TRUE))
   return( dataset)
-  
-options(warn=-1)
-library(foreign)
-  dataset = suppressWarnings(read.spss(file.path(getwd(),"Testing/Cirque.sav"), to.data.frame=TRUE))
-return( dataset)
-
-
 
 }
 
 numero1 <- function() {
   cirque = setup()
-  summary(cirque)
+  cirque2 = cirque[,c(19,6,7 ,18)]
+  library(psych)
+  describe(cirque2,fast=TRUE)
   
 }
 
-numero2 <- function() { 
+numero2 <- function(i) { 
+  cirque = setup()
+  library(ggplot2)
+  if(i==1){
+  g1<- qplot(cirque$NombreJoursVente,cirque$Billets ,
+            data = cirque,
+            geom= c("point","smooth"),
+            method= "lm")
+  
+  print("Nb jour de vente - Billets")
+  g1
+  }else if(i==2){
+    g2<- qplot(cirque$NbPerformances,cirque$Billets ,
+              data = cirque,
+              geom= c("point","smooth"),
+              method= "lm")
+    print("Nb de performances - Billets")
+    g2
+    }else if(i==3){
+      g3<- qplot(cirque$Marketing,cirque$Billets ,
+              data = cirque,
+              geom= c("point","smooth"),
+              method= "lm")
+      print("Total marketing - Billets")
+      g3
+    }  
+  
+  
   
 }
-numero2a <- function() { 
-
-}  
-
+numero3 <- function() {
+  cirque = setup()
+  colnames(cirque)
+  cirque2 = cirque[,c(9,16)]
+  library(psych)
+  describe(cirque2,fast=TRUE)
+  
+}
