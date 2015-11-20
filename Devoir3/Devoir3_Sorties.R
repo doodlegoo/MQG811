0fada354bace78e7cfd22b1c8e1736b526b1a2df
doodlegoo <vincent.gagnon5@usherbrooke.ca>
2015-11-20 03:45:17 -0500

    Devoir 3 presque terminé

--- Devoir3/Devoir3_Sorties.R
+++ Devoir3/Devoir3_Sorties.R
@@ -1,9 +1,18 @@
 setup <- function() {
   options(warn=-1)
   library(foreign)
-  #dataset = suppressWarnings(read.spss(file.path(getwd(),"Devoir3/Devoir3_Cirque.sav"), to.data.frame=TRUE))
+
+  # Pour windows
+  dataset = suppressWarnings(read.spss("Devoir3_Cirque.sav", to.data.frame=TRUE))
+
+  # Pour l'interne
+  #dataset = suppressWarnings(read.spss(file.path(getwd(),"/Devoir3/Devoir3_Cirque.sav"), to.data.frame=TRUE)) 
+  
+  # Pour Linux
+#  dataset = suppressWarnings(read.spss("/home/vincent/Github/mqg811/Devoir3/Devoir3_Cirque.sav", to.data.frame=TRUE))
+
 #  dataset = suppressWarnings(read.spss("~/Rstudio/STT811/mqg811/Devoir3/Devoir3_Cirque.sav", to.data.frame=TRUE))
-  dataset = suppressWarnings(read.spss("/home/vincent/Github/mqg811/Devoir3/Devoir3_Cirque.sav", to.data.frame=TRUE))
+  
   return( dataset)
 
 }
@@ -46,11 +55,102 @@ numero2 <- function(i) {
   
   
 }
-numero3 <- function() {
+statsDescPart2 <- function() {
   cirque = setup()
   colnames(cirque)
   cirque2 = cirque[,c(9,16)]
   library(psych)
+  print("Statistiques descriptives")
   describe(cirque2,fast=TRUE)
   
 }
+
+correlation <- function(){
+  cirque = setup()
+  d <- data.frame(Billets=(cirque$Billets),
+                  JourVente=(cirque$NombreJoursVente),
+                  NbPerformance=(cirque$NbPerformances),
+                  Marketing=(cirque$Marketing))
+  print("Corrélation de pearson")
+  cor(d) # get correlations (returns matrix)
+}
+
+correlation2 <- function(){
+  cirque = setup()
+  d <-data.frame(Billets=(cirque$Billets),
+                 JourDeVente=cirque$NombreJoursVente,
+                 NbPerformance=cirque$NbPerformances,
+                 Orientation=cirque$LongTermOrientation,
+                 Appreciation=cirque$AppreciationShow)
+  print("Corrélation de pearson")
+  cor(d) # get correlations (returns matrix)
+}
+
+stats <- function(){
+  cirque = setup()
+  require(ggplot2)
+  require(car)
+  require(fBasics)
+#   VarDependante = cirque$Billets
+#   if(i==1){
+#     VarIndependante = cirque$NombreJoursVente
+#     titre = "Nb de jour de vente - Billets"
+#   }
+#   if(i==2){
+#     VarIndependante = cirque$NbPerformances
+#     titre = "Nb de performances- Billets"
+#   }
+#   if(i==3){
+#     VarIndependante = cirque$LongTermOrientation
+#     titre = "Orientation long terme - Billets"
+#   }
+#   if(i==4){
+#     VarIndependante = cirque$AppreciationShow
+#     titre = "Appreciation du show - Billets"
+#   }
+#   print(titre)
+#   
+#   R = cor(VarDependante,VarIndependante)
+#   print("R =")
+#   print(R)
+#   print("R-deux =")
+#   print(R*R)
+  VarIndependante = cirque$Billets
+  #VarDependante = cirque$NombreJoursVente +cirque$NbPerformances+ cirque$LongTermOrientation+ cirque$AppreciationShow
+  
+  
+  graph<- qplot(VarIndependante,cirque$NombreJoursVente +cirque$NbPerformances+ cirque$LongTermOrientation+ cirque$AppreciationShow ,
+             data = cirque,
+             geom= c("point","smooth"),
+             method= "lm")
+  
+
+  #lm(VarIndependante ~ VarDependante, data = cirque)
+  
+  R2 =  0.5602
+  print("R =")
+  print(sqrt(R2))
+  print("R-deux =")
+  print(R2)
+  table = lm(VarIndependante ~ cirque$NombreJoursVente +cirque$NbPerformances+ cirque$LongTermOrientation+ cirque$AppreciationShow , data = cirque)
+  print("Récapitulatif des modèles et Coefficients")
+  print(summary(table))
+  print("Intervals de confiance pour les Coefficients")
+  print(confint(table))
+  print("ANOVA - ligne par ligne")
+  print(summary(aov(table)))
+  print(graph)
+  print("VIF")
+  print(vif(table))
+  
+  print("Test de normalité")
+  
+  table.res = resid(table)
+  print(lillieTest(table.res))
+  print(shapiro.test(table.res))
+  
+  g =plot(table.res/sd(table.res))
+  abline(0, 0) 
+  print(g)  
+}
+
